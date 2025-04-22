369d1bd72b4d6c48e37084707348e3a672232a71
Louiscebogoss <louis.fache@lyceedesflandres.fr>
2025-04-22 15:29:58 +0200

    Add files via upload

---
+++ Slimy/entité.py
@@ -0,0 +1,35 @@
+
+
+class Joueur:
+	def __init__(pv = 100, att = 1, spd = 1, head = None, body = None, first_arm = None, second_arm = None, first_leg = None, second_leg = None, etat = None, pos):
+		self.pv = pv
+		self.att = att
+		self.spd = spd
+		self.head = head
+		self.body = body
+		self.first_arm = first_arm
+		self.second_arm = second_arm
+		self.first_leg = first_leg
+		self.second_leg = second_leg
+		self.etat = etat
+		self.pos = pos
+
+class Ennemie:
+	def __init__(pv = 100, att_dist = 1, att_cac = 1, pos):
+		self.pv = pv
+		self.att_dist = att_dist
+		self.att_cac = att_cac
+		self.pos = pos
+
+
+class Boss:
+	def __init__(name, pv = 100, att_dist = 1, att_cac = 1, pos, loot):
+		self.name = name
+		self.pv = pv
+		self.att_dist = att_dist
+		self.att_cac = att_cac
+		self.pos = pos
+		self.loot = None
+
+
+
