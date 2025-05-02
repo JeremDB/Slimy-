9bf1dbfd9f44045c20c6a9c6176f83a6797f760c
Jérémie <jeremie.dufour-blancke@lyceedesflandres.fr>
2025-05-02 10:25:04 +0200

    Update level.py

--- Slimy/level.py
+++ Slimy/level.py
@@ -10,5 +10,26 @@ class Decors:
 	def get_pos(self):
 		return [self.__x, self.__y]
 
+	def get_largeur(self):
+		return self.__largeur
+
+	def get_hauteur(self):
+		return self.__hauteur
+
+	def couleur(self):
+		return self.__couleur
+
 	def scroll(self, n):
 		return [self.__x + n, self.__y]
+
+class Levels:
+
+	def __init__(self,decors,type = None):
+		self.decors = decors
+		self.type = "Normal"
+
+	def get_decors(self):
+		return self.decors
+
+decors = [Decors(0,700,1920,380,"terre"),Decors(900,500,450,50,"terre")]
+lvl = Levels(decors)
