f6710d067c2fd7ec1a0fb3c1ced2937513b649f3
Jérémie <jeremie.dufour-blancke@lyceedesflandres.fr>
2025-05-02 11:51:13 +0200

    Update editor.py

--- Slimy/editor.py
+++ Slimy/editor.py
@@ -1,1 +1,101 @@
 
+import os
+import sys
+import pgzrun
+import pygame
+import level
+from random import randint, choice
+
+WIDTH = 1920
+HEIGHT = 1080
+TITLE = "Slimy!"
+
+class Couleurs():
+    """
+    Propose une méthode permettant de renvoyer une couleur aléatoire
+    Contient les constantes des couleurs utilisés
+    """
+    fond = (150,207,255)
+    terre = (150,90,20)
+    pics = (150,150,150)
+
+    def color(c):
+        if c == "terre":
+            return Couleurs.terre
+        if c == "pics":
+            return Couleurs.pics
+
+    @staticmethod
+    def random() -> tuple:
+        """
+        Renvoie une couleur aléatoire au format (R,G,B)
+        """
+        return (randint(0,255),randint(0,255),randint(0,255))
+
+def update():
+    """
+    """
+    check_keys()
+
+def draw():
+    """
+    """
+    screen.fill(Couleurs.fond)
+    draw_levels()
+
+def draw_levels():
+    """
+    """
+    for lvl in levels :
+        for decor in lvl.get_decors():
+            draw_decor(decor)
+
+def draw_decor(decor):
+    topleft = decor.get_pos()
+    largeur = decor.get_largeur()
+    hauteur = decor.get_hauteur()
+    couleur = Couleurs.color(decor.couleur())
+    rect_decor = Rect(topleft,(largeur,hauteur))
+    screen.draw.filled_rect(rect_decor,couleur)
+
+def ajoute_decors(nv):
+    global levels
+    levels[0].add_decor(level.Decors(nv["x"],nv["y"],nv["largeur"],nv["hauteur"],"terre"))
+
+def on_mouse_down(pos, button):
+    global topleft,bottomright,decors
+    if button == mouse.LEFT :
+        topleft = pos
+    if button == mouse.RIGHT :
+        bottomright = pos 
+    if button == mouse.MIDDLE and topleft != None and bottomright != None:
+        nv = {
+            "x" : topleft[0],
+            "y" : topleft[1],
+            "largeur" : bottomright[0] - topleft[0],
+            "hauteur" : bottomright[1] - topleft[1]
+            }
+        decors.append(nv)
+        ajoute_decors(nv)
+
+def check_keys():
+    """
+    Verifie les touches enfoncée 
+    Echap pour quitter le jeu
+    """
+    global topleft, bottomright
+    if keyboard.C :
+        topleft = None
+        bottomright = None
+    # Echap pour quitter le jeu
+    if keyboard.ESCAPE:
+        print(decors)
+        exit()
+
+levels = level.nouveau_lvl
+topleft = None
+bottomright = None
+decors=[]
+print(decors)
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+pgzrun.go()
