db85e377fdb926f3144006a8068d07bad53e3ce3
Louiscebogoss <louis.fache@lyceedesflandres.fr>
2025-05-06 15:30:36 +0200

    Update game.py

--- Slimy/game.py
+++ Slimy/game.py
@@ -23,8 +23,8 @@ class Couleurs():
 
 
     def color(c):
-    	if c == "terre":
-    		return Couleurs.terre
+        if c == "terre":
+            return Couleurs.terre
 
     @staticmethod
     def random() -> tuple:
@@ -34,64 +34,82 @@ class Couleurs():
         return (randint(0,255),randint(0,255),randint(0,255))
 
 def update():
-	"""
-	"""
-	check_keys()
-	if check_collisisons_down() == False:
-		gravite()
+    global force_gravite, w, count_frame
+    """
+    """
+    count_frame += 1
 
+    check_keys()
+
+    if player.etat == "saut":
+        gravite()
+    if check_collisisons_down() == -1:
+        calc_speed()
+        gravite()
+    if check_collisisons_down() == w:
+        player.pos[1] = w - hauteur_player
+        player.etat = None
+
+    
 def check_collisisons_down():
-	global force_gravitationnel
-	x = player.pos[0]
-	y = player.pos[1] + force_gravitationnel
-	rect_player = Rect((x,y),(50,40))
-	for lvl in levels :
-		for decor in lvl.get_decors():
-			v = decor.get_pos()[0]
-			w = decor.get_pos()[1]
-			largeur = decor.get_largeur()
-			hauteur = decor.get_hauteur()
-			rect_decor = Rect((v,w),(largeur,hauteur))
-			return pygame.Rect.colliderect(rect_player,rect_decor)
-
+    global w 
+    x = player.pos[0]
+    y = player.pos[1] + force_gravite
+    rect_player = Rect((x,y),(50,40))
+    for lvl in levels :
+        for decor in lvl.get_decors():
+            v = decor.get_pos()[0]
+            w = decor.get_pos()[1]
+            largeur = decor.get_largeur()
+            hauteur = decor.get_hauteur()
+            rect_decor = Rect((v,w),(largeur,hauteur))
+            if pygame.Rect.colliderect(rect_player,rect_decor):
+                return w
+        return -1
 
 def gravite():
-	global force_gravitationnel
-	player.pos[1] += force_gravitationnel 
+    global force_gravite
+    player.pos[1] += force_gravite
 
-
+def calc_speed():
+  global force_gravite
+  v_y = force_gravite
+  v_y = v_y + 0.10*gravity
+  force_gravite = v_y
+  return
 
 def draw():
-	"""
-	"""
-	screen.fill(Couleurs.fond)
-	rect = Rect((500,800),(300,60))
-	draw_levels()
-	screen.draw.filled_rect(rect,(Couleurs.terre))
-	draw_player()
+    """
+    """
+    screen.fill(Couleurs.fond)
+    rect = Rect((500,800),(300,60))
+    draw_levels()
+    screen.draw.filled_rect(rect,(Couleurs.terre))
+    draw_player()
 
 def draw_player():
-	x = player.pos[0]
-	y = player.pos[1]
-	rect = Rect((x,y),(50,40))
-	screen.draw.filled_rect(rect,Couleurs.slime)
+    x = player.pos[0]
+    y = player.pos[1]
+    rect = Rect((x,y),(50,hauteur_player))
+    screen.draw.filled_rect(rect,Couleurs.slime)
 
 def draw_levels():
-	"""
-	"""
-	for lvl in levels :
-		for decor in lvl.get_decors():
-			draw_decor(decor)
+    """
+    """
+    for lvl in levels :
+        for decor in lvl.get_decors():
+            draw_decor(decor)
 
 def draw_decor(decor):
-	topleft = decor.get_pos()
-	largeur = decor.get_largeur()
-	hauteur = decor.get_hauteur()
-	couleur = Couleurs.color(decor.couleur())
-	rect_decor = Rect(topleft,(largeur,hauteur))
-	screen.draw.filled_rect(rect_decor,couleur)
+    topleft = decor.get_pos()
+    largeur = decor.get_largeur()
+    hauteur = decor.get_hauteur()
+    couleur = Couleurs.color(decor.couleur())
+    rect_decor = Rect(topleft,(largeur,hauteur))
+    screen.draw.filled_rect(rect_decor,couleur)
 
 def check_keys():
+    global count_frame,force_gravite
     """
     Verifie les touches enfoncée 
     Echap pour quitter le jeu
@@ -101,11 +119,23 @@ def check_keys():
     if keyboard.ESCAPE:
         exit()
     if keyboard.SPACE:
-    	player.pos[1] -= 100
+        if count_frame > 10 and player.etat != "saut":
+            force_gravite = -1
+            player.etat = "saut"
+            force_gravite -= 4
+            count_frame = 0
+    if keyboard.D:
+        player.pos[0] += 5
+    if keyboard.Q:
+        player.pos[0] -= 5
 
 levels = [level.lvl]
 player = entite.Joueur([50,660])
-force_gravitationnel = 10
+gravity = 1
+force_gravite = -1 
+hauteur_player = 40
+w = 0
+count_frame = 0
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 pgzrun.go()
