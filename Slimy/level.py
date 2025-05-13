8acac8c925c14ff8daccc66e4a0619daae1f37b7
Jérémie <jeremie.dufour-blancke@lyceedesflandres.fr>
2025-05-13 15:08:00 +0200

    Update level.py

--- Slimy/level.py
+++ Slimy/level.py
@@ -50,6 +50,7 @@ class Levels:
 
 niveau1 = Levels([Decors(0, 700, 80, 380, 'terre'),Decors(1840, 700, 80, 380, 'terre'),Decors(80, 700, 880, 380, 'terre'),Decors(320, 560, 320, 20, 'terre'),Decors(960, 700, 400, 380, 'terre'),Decors(1280, 620, 320, 480, 'terre'),Decors(1440, 580, 280, 520, 'terre'),Decors(640, 480, 240, 20, 'terre'),Decors(960, 440, 160, 20, 'terre'),Decors(1200, 360, 160, 20, 'terre')])
 	
+niveaux_boss = [Levels([Decors(0, 700, 80, 380, 'terre'),Decors(1840, 700, 80, 380, 'terre'),Decors(80, 660, 500, 480, 'terre'),Decors(480, 760, 400, 340, 'terre'),Decors(880, 780, 900, 320, 'terre'),Decors(640, 560, 160, 20, 'terre'),Decors(800, 680, 80, 100, 'terre'),Decors(800, 560, 160, 20, 'terre'),Decors(880, 440, 160, 20, 'terre'),Decors(1040, 320, 240, 20, 'terre'),Decors(1040, 560, 160, 20, 'terre')	])]
 
 niveaux = [
 	Levels([Decors(0, 700, 80, 380, 'terre'),Decors(1840, 700, 80, 380, 'terre'),Decors(0, 700, 400, 420, 'terre'),Decors(880, 700, 480, 380, 'terre'),Decors(320, 860, 240, 20, 'terre'),Decors(640, 800, 160, 20, 'terre'),Decors(800, 740, 160, 20, 'terre'),Decors(1360, 840, 160, 20, 'terre'),Decors(1520, 780, 80, 20, 'terre'),Decors(1680, 740, 160, 20, 'terre'),Decors(320, 520, 240, 20, 'terre'),Decors(640, 460, 240, 20, 'terre'),Decors(960, 380, 240, 20, 'terre'),Decors(1360, 360, 240, 20, 'terre')]),
@@ -68,24 +69,29 @@ def ajuste_niveaux(levels):
 	levels.pop(0)
 	return levels
 
+def ajuste_niveaux_boss(levels):
+	nouveau = deepcopy(niveaux_boss[randint(0,len(niveaux_boss)-1)])
+	nouveau.decalage()
+	levels.append(nouveau)
+	levels.pop(0)
+	return levels
+
 
 
 nouveau_lvl = [
 Levels([Decors(0, 700, 80, 380, 'terre'),
 Decors(1840, 700, 80, 380, 'terre'),
-Decors(-80, 640, 560, 480, 'terre'),
-Decors(240, 580, 480, 480, 'terre'),
-Decors(640, 620, 400, 480, 'terre'),
-Decors(1040, 800, 320, 300, 'terre'),
-Decors(960, 700, 160, 400, 'terre'),
-Decors(1440, 780, 160, 20, 'terre'),
-Decors(1680, 780, 160, 20, 'terre'),
-Decors(960, 740, 240, 600, 'terre'),
-Decors(380, 540, 480, 600, 'terre'),
-Decors(420,560,200,600, 'terre')
+Decors(80, 660, 500, 480, 'terre'),
+Decors(480, 760, 400, 340, 'terre'),
+Decors(880, 780, 900, 320, 'terre'),
+Decors(640, 560, 160, 20, 'terre'),
+Decors(800, 680, 80, 100, 'terre'),
+Decors(800, 560, 160, 20, 'terre'),
+Decors(880, 440, 160, 20, 'terre'),
+Decors(1040, 320, 240, 20, 'terre'),
+Decors(1040, 560, 160, 20, 'terre')
 	])
 	]
 nb_niveau = len(niveaux)
 decors_de_base = [Decors(0,700,80,380,"terre"),Decors(1840,700,80,380,"terre")]
 niveau_vide = [Levels(decors_de_base)]
-
