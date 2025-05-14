73d6c9d7523dfa59d292fadd5649a74d3de39cda
Jérémie <jeremie.dufour-blancke@lyceedesflandres.fr>
2025-05-14 16:32:54 +0200

    Update editor.py

--- Slimy/editor.py
+++ Slimy/editor.py
@@ -1,118 +1,111 @@
+from random import randint
+from copy import deepcopy
 
-import os
-import sys
-import pgzrun
-import pygame
-import level
-from random import randint, choice
+class Decors:
 
-WIDTH = 1920
-HEIGHT = 1080
-TITLE = "Slimy!"
+	def __init__(self, x, y, largeur, hauteur, couleur):
+		self.__x = x
+		self.__y = y
+		self.__largeur = largeur
+		self.__hauteur = hauteur
+		self.__couleur = couleur 
 
-class Couleurs():
-    """
-    Propose une méthode permettant de renvoyer une couleur aléatoire
-    Contient les constantes des couleurs utilisés
-    """
-    fond = (150,207,255)
-    terre = (150,90,20)
-    pics = (150,150,150)
+	def get_pos(self):
+		return [self.__x, self.__y]
 
-    def color(c):
-        if c == "terre":
-            return Couleurs.terre
-        if c == "pics":
-            return Couleurs.pics
+	def get_largeur(self):
+		return self.__largeur
 
-    @staticmethod
-    def random() -> tuple:
-        """
-        Renvoie une couleur aléatoire au format (R,G,B)
-        """
-        return (randint(0,255),randint(0,255),randint(0,255))
+	def get_hauteur(self):
+		return self.__hauteur
 
-def update():
-    """
-    """
-    check_keys()
+	def couleur(self):
+		return self.__couleur
 
-def draw():
-    """
-    """
-    screen.fill(Couleurs.fond)
-    draw_levels()
+	def scroll(self, n):
+		self.__x -= n
 
-def draw_levels():
-    """
-    """
-    for lvl in levels :
-        for decor in lvl.get_decors():
-            draw_decor(decor)
+	def __repr__(self):
+		return f"{self.__x,self.__y,self.__largeur,self.__hauteur,self.__couleur}"
 
-def draw_decor(decor):
-    topleft = decor.get_pos()
-    largeur = decor.get_largeur()
-    hauteur = decor.get_hauteur()
-    couleur = Couleurs.color(decor.couleur())
-    rect_decor = Rect(topleft,(largeur,hauteur))
-    screen.draw.filled_rect(rect_decor,couleur)
+class Levels:
 
-def ajoute_decors(nv):
-    global levels
-    levels[0].add_decor(level.Decors(nv["x"],nv["y"],nv["largeur"],nv["hauteur"],"terre"))
+	def __init__(self,decors,type = None):
+		self.pos_x = 1920
+		self.decors = decors
+		self.type = "Normal"
 
-def on_mouse_down(pos, button):
-    global topleft,bottomright,decors
-    if button == mouse.LEFT :
-        topleft = pos
-    if button == mouse.RIGHT :
-        bottomright = pos 
-    if button == mouse.MIDDLE and topleft != None and bottomright != None:
-        nv = {
-            "x" : topleft[0],
-            "y" : topleft[1],
-            "largeur" : bottomright[0] - topleft[0],
-            "hauteur" : bottomright[1] - topleft[1]
-            }
-        decors.append(nv)
-        ajoute_decors(nv)
+	def get_decors(self):
+		return self.decors
 
-def check_keys():
-    """
-    Verifie les touches enfoncée 
-    Echap pour quitter le jeu
-    """
-    global topleft, bottomright
-    if keyboard.C :
-        topleft = None
-        bottomright = None
-    # Echap pour quitter le jeu
-    if keyboard.ESCAPE:
-        liste_decors()
-        exit()
+	def add_decor(self,decor):
+		self.decors.append(decor)
 
+	def scroll(self,n):
+		self.pos_x -= n
+		for decor in self.decors:
+			decor.scroll(n)
 
-def liste_decors() :
-    nouveau_decors = [level.Decors(0,700,80,380,"terre"),level.Decors(1840,700,80,380,"terre")]
-    for decor in decors :  
-        decor["x"] = ((decor["x"] // 80) - 1) * 80
-        decor["y"] = ((decor["y"] // 20) - 1) * 20  
-        decor["largeur"] = ((decor["largeur"] // 80) + 1) * 80
-        decor["hauteur"] = ((decor["hauteur"] // 20) + 1) * 20
-        nouveau_decors.append(level.Decors(decor["x"], decor["y"],decor["largeur"],decor["hauteur"],"terre"))
+	def decalage(self,pos):
+		decal = -1920 - pos
+		for decor in self.decors :
+			decor.scroll(decal)
 
-    for decor in nouveau_decors : 
-        print(f"Decors{decor},")
+niveau1 = Levels(
+	[Decors(-80, 700, 160, 400, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(320, 580, 320, 20, 'decor'),Decors(640, 480, 240, 20, 'decor'),Decors(960, 440, 160, 20, 'decor'),Decors(1200, 360, 160, 20, 'decor'),Decors(80, 700, 880, 400, 'decor'),Decors(960, 700, 400, 400, 'decor'),Decors(1280, 620, 320, 480, 'decor'),Decors(1440, 580, 280, 520, 'decor')])
+	
+niveaux_boss = [
+Levels(
+	[Decors(-80, 700, 160, 400, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(800, 680, 80, 100, 'decor'),Decors(80, 660, 480, 480, 'decor'),Decors(560, 760, 320, 340, 'decor'),Decors(880, 780, 900, 320, 'decor'),Decors(640, 560, 280, 20, 'decor'),Decors(880, 440, 160, 20, 'decor'),Decors(1040, 320, 240, 20, 'decor'),Decors(1040, 560, 160, 20, 'decor')]
+	)
+]
+
+niveaux = [
+#Niveau 1 
+Levels([
+	Decors(-80, 700, 160, 400, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(320, 860, 240, 20, 'decor'),Decors(320, 560, 240, 20, 'decor'),Decors(640, 800, 160, 20, 'decor'),Decors(640, 460, 240, 20, 'decor'),Decors(800, 740, 160, 20, 'decor'),Decors(960, 380, 240, 20, 'decor'),Decors(1320, 840, 160, 20, 'decor'),Decors(1520, 780, 80, 20, 'decor'),Decors(1680, 740, 200, 20, 'decor'),Decors(1360, 360, 240, 20, 'decor'),Decors(80, 700, 400, 420, 'decor'),Decors(880, 700, 480, 380, 'decor')]
+	),
+#Niveau 2
+Levels([
+	Decors(-80, 700, 160, 380, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(1440, 780, 160, 20, 'decor'),Decors(1680, 780, 200, 20, 'decor'),Decors(1040, 800, 320, 300, 'decor'),Decors(960, 740, 240, 600, 'decor'),Decors(80, 700, 480, 480, 'decor'),Decors(960, 700, 160, 400, 'decor'),Decors(640, 620, 400, 480, 'decor'),Decors(240, 580, 480, 520, 'decor'),Decors(380, 540, 480, 600, 'decor')]
+	)
+]
+
+def init_niveau():
+	niv2 = deepcopy(niveaux[randint(0,len(niveaux)-1)])
+	niv2.decalage(0)
+	return [niveau1, niv2]
+
+def ajuste_niveaux(levels,pos):
+	nouveau = deepcopy(niveaux[randint(0,len(niveaux)-1)])
+	nouveau.decalage(pos)
+	levels.append(nouveau)
+	levels.pop(0)
+	return levels
+
+def ajuste_niveaux_boss(levels,pos):
+	nouveau = deepcopy(niveaux_boss[randint(0,len(niveaux_boss)-1)])
+	nouveau.decalage(pos)
+	levels.append(nouveau)
+	levels.pop(0)
+	return levels
 
 
 
-
-#levels = level.niveau_vide
-levels = level.nouveau_lvl
-topleft = None
-bottomright = None
-decors=[]
-print(decors)
-os.environ['SDL_VIDEO_CENTERED'] = '1'
-pgzrun.go()
+nouveau_lvl = [
+Levels([
+	Decors(1440, 780, 160, 20, 'decor')
+	,Decors(1680, 780, 200, 20, 'decor')
+	,Decors(1040, 800, 320, 300, 'decor')
+	,Decors(960, 740, 240, 600, 'decor')
+	,Decors(1840, 700, 80, 380, 'decor')
+	,Decors(-40, 700, 120, 380, 'decor')
+	,Decors(80, 700, 480, 480, 'decor')
+	,Decors(960, 700, 160, 400, 'decor')
+	,Decors(640, 620, 400, 480, 'decor')
+	,Decors(240, 580, 480, 520, 'decor')
+	,Decors(380, 540, 480, 600, 'decor')])
+]
+nb_niveau = len(niveaux)
+decors_de_base = [Decors(0,700,80,380,"decor"),Decors(1840,700,80,380,"decor")]
+niveau_vide = [Levels(decors_de_base)]
