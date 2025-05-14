0d5363131d24fb31da66ee809e0cedc9ca617c0f
Louiscebogoss <louis.fache@lyceedesflandres.fr>
2025-05-14 11:48:57 +0200

    Update level.py

--- Slimy/level.py
+++ Slimy/level.py
@@ -30,7 +30,7 @@ class Decors:
 
 class Levels:
 
-	def __init__(self,decors,ennemis = None,type = None):
+	def __init__(self,decors,type = None):
 		self.decors = decors
 		self.type = "Normal"
 
@@ -48,13 +48,56 @@ class Levels:
 		for decor in self.decors :
 			decor.scroll(-1920)
 
-niveau1 = Levels([Decors(0, 700, 80, 380, 'decor'),Decors(1840, 700, 80, 380, 'decor'),Decors(80, 700, 880, 380, 'decor'),Decors(320, 560, 320, 20, 'decor'),Decors(960, 700, 400, 380, 'decor'),Decors(1280, 620, 320, 480, 'decor'),Decors(1440, 580, 280, 520, 'decor'),Decors(640, 480, 240, 20, 'decor'),Decors(960, 440, 160, 20, 'decor'),Decors(1200, 360, 160, 20, 'decor')])
+niveau1 = Levels([Decors(-40, 700, 120, 380, 'decor')
+	,Decors(1840, 700, 80, 380, 'decor')
+,Decors(80, 700, 880, 380, 'decor')
+,Decors(320, 580, 320, 20, 'decor')
+,Decors(960, 700, 400, 380, 'decor')
+,Decors(1280, 620, 320, 480, 'decor')
+,Decors(1440, 580, 280, 520, 'decor')
+,Decors(640, 480, 240, 20, 'decor')
+,Decors(960, 440, 160, 20, 'decor')
+,Decors(1200, 360, 160, 20, 'decor')])
 	
-niveaux_boss = [Levels([Decors(0, 700, 80, 380, 'decor'),Decors(1840, 700, 80, 380, 'decor'),Decors(80, 660, 500, 480, 'decor'),Decors(480, 760, 400, 340, 'decor'),Decors(880, 780, 900, 320, 'decor'),Decors(640, 560, 160, 20, 'decor'),Decors(800, 680, 80, 100, 'decor'),Decors(800, 560, 160, 20, 'decor'),Decors(880, 440, 160, 20, 'decor'),Decors(1040, 320, 240, 20, 'decor'),Decors(1040, 560, 160, 20, 'decor')	], type= "boss")]
+niveaux_boss = [Levels([Decors(-40, 700, 120, 380, 'decor')
+	,Decors(1840, 700, 80, 380, 'decor')
+,Decors(800, 680, 80, 100, 'decor')
+,Decors(80, 660, 500, 480, 'decor')
+,Decors(480, 760, 400, 340, 'decor')
+,Decors(880, 780, 900, 320, 'decor')
+,Decors(640, 560, 160, 20, 'decor')
+,Decors(800, 560, 160, 20, 'decor')
+,Decors(880, 440, 160, 20, 'decor')
+,Decors(1040, 320, 240, 20, 'decor')
+,Decors(1040, 560, 160, 20, 'decor')	])]
 
 niveaux = [
-	Levels([Decors(0, 700, 80, 380, 'decor'),Decors(1840, 700, 80, 380, 'decor'),Decors(0, 700, 400, 420, 'decor'),Decors(880, 700, 480, 380, 'decor'),Decors(320, 860, 240, 20, 'decor'),Decors(640, 800, 160, 20, 'decor'),Decors(800, 740, 160, 20, 'decor'),Decors(1360, 840, 160, 20, 'decor'),Decors(1520, 780, 80, 20, 'decor'),Decors(1680, 740, 160, 20, 'decor'),Decors(320, 520, 240, 20, 'decor'),Decors(640, 460, 240, 20, 'decor'),Decors(960, 380, 240, 20, 'decor'),Decors(1360, 360, 240, 20, 'decor')]),
-	Levels([Decors(0, 700, 80, 380, 'decor'),Decors(1840, 700, 80, 380, 'decor'),Decors(80, 700, 480, 480, 'decor'),Decors(240, 580, 480, 480, 'decor'),Decors(640, 620, 400, 480, 'decor'),Decors(1040, 800, 320, 300, 'decor'),Decors(960, 700, 160, 400, 'decor'),Decors(1440, 780, 160, 20, 'decor'),Decors(1680, 780, 160, 20, 'decor'),Decors(960, 740, 240, 600, 'decor'),Decors(380, 540, 480, 600, 'decor'),Decors(420,560,200,600, 'decor')])
+	Levels([Decors(-40, 700, 120, 380, 'decor')
+		,Decors(1840, 700, 80, 380, 'decor')
+	,Decors(0, 700, 400, 420, 'decor')
+	,Decors(880, 700, 480, 380, 'decor')
+	,Decors(320, 860, 240, 20, 'decor')
+	,Decors(640, 800, 160, 20, 'decor')
+	,Decors(800, 740, 160, 20, 'decor')
+	,Decors(1360, 840, 160, 20, 'decor')
+	,Decors(1520, 780, 80, 20, 'decor')
+	,Decors(1680, 740, 160, 20, 'decor')
+	,Decors(320, 560, 240, 20, 'decor')
+	,Decors(640, 460, 240, 20, 'decor')
+	,Decors(960, 380, 240, 20, 'decor')
+	,Decors(1360, 360, 240, 20, 'decor')]),
+	Levels([Decors(-40, 700, 120, 380, 'decor')
+		,Decors(1840, 700, 80, 380, 'decor')
+	,Decors(80, 700, 480, 480, 'decor')
+	,Decors(240, 580, 480, 480, 'decor')
+	,Decors(640, 620, 400, 480, 'decor')
+	,Decors(1040, 800, 320, 300, 'decor')
+	,Decors(960, 700, 160, 400, 'decor')
+	,Decors(1440, 780, 160, 20, 'decor')
+	,Decors(1680, 780, 160, 20, 'decor')
+	,Decors(960, 740, 240, 600, 'decor')
+	,Decors(380, 540, 480, 600, 'decor')
+	,Decors(420,560,200,600, 'decor')])
 	]
 
 def init_niveau():
