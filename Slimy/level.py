9df1e04ba6d949d36734951663beb402674943b1
Jérémie <jeremie.dufour-blancke@lyceedesflandres.fr>
2025-05-14 16:32:39 +0200

    Update level.py

--- Slimy/level.py
+++ Slimy/level.py
@@ -31,6 +31,7 @@ class Decors:
 class Levels:
 
 	def __init__(self,decors,type = None):
+		self.pos_x = 1920
 		self.decors = decors
 		self.type = "Normal"
 
@@ -41,80 +42,50 @@ class Levels:
 		self.decors.append(decor)
 
 	def scroll(self,n):
+		self.pos_x -= n
 		for decor in self.decors:
 			decor.scroll(n)
 
-	def decalage(self):
+	def decalage(self,pos):
+		decal = -1920 - pos
 		for decor in self.decors :
-			decor.scroll(-1920)
+			decor.scroll(decal)
 
-niveau1 = Levels([Decors(-40, 700, 120, 380, 'decor')
-	,Decors(1840, 700, 80, 380, 'decor')
-,Decors(80, 700, 880, 380, 'decor')
-,Decors(320, 580, 320, 20, 'decor')
-,Decors(960, 700, 400, 380, 'decor')
-,Decors(1280, 620, 320, 480, 'decor')
-,Decors(1440, 580, 280, 520, 'decor')
-,Decors(640, 480, 240, 20, 'decor')
-,Decors(960, 440, 160, 20, 'decor')
-,Decors(1200, 360, 160, 20, 'decor')])
+niveau1 = Levels(
+	[Decors(-80, 700, 160, 400, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(320, 580, 320, 20, 'decor'),Decors(640, 480, 240, 20, 'decor'),Decors(960, 440, 160, 20, 'decor'),Decors(1200, 360, 160, 20, 'decor'),Decors(80, 700, 880, 400, 'decor'),Decors(960, 700, 400, 400, 'decor'),Decors(1280, 620, 320, 480, 'decor'),Decors(1440, 580, 280, 520, 'decor')])
 	
-niveaux_boss = [Levels([Decors(-40, 700, 120, 380, 'decor')
-	,Decors(1840, 700, 80, 380, 'decor')
-,Decors(800, 680, 80, 100, 'decor')
-,Decors(80, 660, 500, 480, 'decor')
-,Decors(480, 760, 400, 340, 'decor')
-,Decors(880, 780, 900, 320, 'decor')
-,Decors(640, 560, 160, 20, 'decor')
-,Decors(800, 560, 160, 20, 'decor')
-,Decors(880, 440, 160, 20, 'decor')
-,Decors(1040, 320, 240, 20, 'decor')
-,Decors(1040, 560, 160, 20, 'decor')	])]
+niveaux_boss = [
+Levels(
+	[Decors(-80, 700, 160, 400, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(800, 680, 80, 100, 'decor'),Decors(80, 660, 480, 480, 'decor'),Decors(560, 760, 320, 340, 'decor'),Decors(880, 780, 900, 320, 'decor'),Decors(640, 560, 280, 20, 'decor'),Decors(880, 440, 160, 20, 'decor'),Decors(1040, 320, 240, 20, 'decor'),Decors(1040, 560, 160, 20, 'decor')]
+	)
+]
 
 niveaux = [
-	Levels([Decors(-40, 700, 120, 380, 'decor')
-		,Decors(1840, 700, 80, 380, 'decor')
-	,Decors(0, 700, 400, 420, 'decor')
-	,Decors(880, 700, 480, 380, 'decor')
-	,Decors(320, 860, 240, 20, 'decor')
-	,Decors(640, 800, 160, 20, 'decor')
-	,Decors(800, 740, 160, 20, 'decor')
-	,Decors(1360, 840, 160, 20, 'decor')
-	,Decors(1520, 780, 80, 20, 'decor')
-	,Decors(1680, 740, 160, 20, 'decor')
-	,Decors(320, 560, 240, 20, 'decor')
-	,Decors(640, 460, 240, 20, 'decor')
-	,Decors(960, 380, 240, 20, 'decor')
-	,Decors(1360, 360, 240, 20, 'decor')]),
-	Levels([Decors(-40, 700, 120, 380, 'decor')
-		,Decors(1840, 700, 80, 380, 'decor')
-	,Decors(80, 700, 480, 480, 'decor')
-	,Decors(240, 580, 480, 480, 'decor')
-	,Decors(640, 620, 400, 480, 'decor')
-	,Decors(1040, 800, 320, 300, 'decor')
-	,Decors(960, 700, 160, 400, 'decor')
-	,Decors(1440, 780, 160, 20, 'decor')
-	,Decors(1680, 780, 160, 20, 'decor')
-	,Decors(960, 740, 240, 600, 'decor')
-	,Decors(380, 540, 480, 600, 'decor')
-	,Decors(420,560,200,600, 'decor')])
-	]
+#Niveau 1 
+Levels([
+	Decors(-80, 700, 160, 400, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(320, 860, 240, 20, 'decor'),Decors(320, 560, 240, 20, 'decor'),Decors(640, 800, 160, 20, 'decor'),Decors(640, 460, 240, 20, 'decor'),Decors(800, 740, 160, 20, 'decor'),Decors(960, 380, 240, 20, 'decor'),Decors(1320, 840, 160, 20, 'decor'),Decors(1520, 780, 80, 20, 'decor'),Decors(1680, 740, 200, 20, 'decor'),Decors(1360, 360, 240, 20, 'decor'),Decors(80, 700, 400, 420, 'decor'),Decors(880, 700, 480, 380, 'decor')]
+	),
+#Niveau 2
+Levels([
+	Decors(-80, 700, 160, 380, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(1440, 780, 160, 20, 'decor'),Decors(1680, 780, 200, 20, 'decor'),Decors(1040, 800, 320, 300, 'decor'),Decors(960, 740, 240, 600, 'decor'),Decors(80, 700, 480, 480, 'decor'),Decors(960, 700, 160, 400, 'decor'),Decors(640, 620, 400, 480, 'decor'),Decors(240, 580, 480, 520, 'decor'),Decors(380, 540, 480, 600, 'decor')]
+	)
+]
 
 def init_niveau():
 	niv2 = deepcopy(niveaux[randint(0,len(niveaux)-1)])
-	niv2.decalage()
+	niv2.decalage(0)
 	return [niveau1, niv2]
 
-def ajuste_niveaux(levels):
+def ajuste_niveaux(levels,pos):
 	nouveau = deepcopy(niveaux[randint(0,len(niveaux)-1)])
-	nouveau.decalage()
+	nouveau.decalage(pos)
 	levels.append(nouveau)
 	levels.pop(0)
 	return levels
 
-def ajuste_niveaux_boss(levels):
+def ajuste_niveaux_boss(levels,pos):
 	nouveau = deepcopy(niveaux_boss[randint(0,len(niveaux_boss)-1)])
-	nouveau.decalage()
+	nouveau.decalage(pos)
 	levels.append(nouveau)
 	levels.pop(0)
 	return levels
@@ -122,19 +93,19 @@ def ajuste_niveaux_boss(levels):
 
 
 nouveau_lvl = [
-Levels([Decors(0, 700, 80, 380, 'decor'),
-Decors(1840, 700, 80, 380, 'decor'),
-Decors(80, 660, 500, 480, 'decor'),
-Decors(480, 760, 400, 340, 'decor'),
-Decors(880, 780, 900, 320, 'decor'),
-Decors(640, 560, 160, 20, 'decor'),
-Decors(800, 680, 80, 100, 'decor'),
-Decors(800, 560, 160, 20, 'decor'),
-Decors(880, 440, 160, 20, 'decor'),
-Decors(1040, 320, 240, 20, 'decor'),
-Decors(1040, 560, 160, 20, 'decor')
-	])
-	]
+Levels([
+	Decors(1440, 780, 160, 20, 'decor')
+	,Decors(1680, 780, 200, 20, 'decor')
+	,Decors(1040, 800, 320, 300, 'decor')
+	,Decors(960, 740, 240, 600, 'decor')
+	,Decors(1840, 700, 80, 380, 'decor')
+	,Decors(-40, 700, 120, 380, 'decor')
+	,Decors(80, 700, 480, 480, 'decor')
+	,Decors(960, 700, 160, 400, 'decor')
+	,Decors(640, 620, 400, 480, 'decor')
+	,Decors(240, 580, 480, 520, 'decor')
+	,Decors(380, 540, 480, 600, 'decor')])
+]
 nb_niveau = len(niveaux)
 decors_de_base = [Decors(0,700,80,380,"decor"),Decors(1840,700,80,380,"decor")]
 niveau_vide = [Levels(decors_de_base)]
