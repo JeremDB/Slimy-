365a32861faec7e6912a39267bad4d615f682f30
Jérémie <jeremie.dufour-blancke@lyceedesflandres.fr>
2025-05-14 17:38:24 +0200

    Update level.py

--- Slimy/level.py
+++ Slimy/level.py
@@ -2,12 +2,13 @@ from random import randint
 from copy import deepcopy
 
 class Ennemi:
-	def __init__(self, pos, taille, pv = 100, att_dist = 1, att_cac = 1):
+	def __init__(self, pos, taille,forme, pv = 100, att_dist = 1, att_cac = 1):
 		self.pv = pv
 		self.att_dist = att_dist
 		self.att_cac = att_cac
 		self.taille = taille
 		self.pos = pos
+		self.type= forme
 
 	def scroll(self, n):
 		self.pos[0] -= n
@@ -77,15 +78,17 @@ class Levels:
 		decal = -1920 - pos
 		for decor in self.decors :
 			decor.scroll(decal)
+		for ennemi in self.ennemis:
+			ennemi.scroll(decal)
 
 niveau1 = Levels(
-	[Decors(-80, 700, 160, 400, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(320, 580, 320, 20, 'decor'),Decors(640, 480, 240, 20, 'decor'),Decors(960, 440, 160, 20, 'decor'),Decors(1200, 360, 160, 20, 'decor'),Decors(80, 700, 880, 400, 'decor'),Decors(960, 700, 400, 400, 'decor'),Decors(1280, 620, 320, 480, 'decor'),Decors(1440, 580, 280, 520, 'decor')]
-	,[Ennemi([1085,660],(40,40))]
+	[Decors(-80, 700, 160, 400, 'decor'),Decors(320, 580, 320, 20, 'decor'),Decors(640, 480, 240, 20, 'decor'),Decors(960, 440, 160, 20, 'decor'),Decors(1200, 360, 160, 20, 'decor'),Decors(80, 700, 880, 400, 'decor'),Decors(960, 700, 400, 400, 'decor'),Decors(1280, 620, 320, 480, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(1440, 580, 280, 520, 'decor')]
+	,[Ennemi([1085,660],(40,40),"rond")]
 	)
 	
 niveaux_boss = [
 Levels(
-	[Decors(-80, 700, 160, 400, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(800, 680, 80, 100, 'decor'),Decors(80, 660, 480, 480, 'decor'),Decors(560, 760, 320, 340, 'decor'),Decors(880, 780, 900, 320, 'decor'),Decors(640, 560, 280, 20, 'decor'),Decors(880, 440, 160, 20, 'decor'),Decors(1040, 320, 240, 20, 'decor'),Decors(1040, 560, 160, 20, 'decor')]
+	[Decors(-80, 700, 160, 400, 'decor'),Decors(800, 680, 80, 100, 'decor'),Decors(80, 660, 480, 480, 'decor'),Decors(560, 760, 320, 340, 'decor'),Decors(880, 780, 900, 320, 'decor'),Decors(640, 560, 280, 20, 'decor'),Decors(880, 440, 160, 20, 'decor'),Decors(1040, 320, 240, 20, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(1040, 560, 160, 20, 'decor')]
 	,[]
 	)
 ]
@@ -93,13 +96,13 @@ Levels(
 niveaux = [
 #Niveau 1 
 Levels([
-	Decors(-80, 700, 160, 400, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(320, 860, 240, 20, 'decor'),Decors(320, 560, 240, 20, 'decor'),Decors(640, 800, 160, 20, 'decor'),Decors(640, 460, 240, 20, 'decor'),Decors(800, 740, 160, 20, 'decor'),Decors(960, 380, 240, 20, 'decor'),Decors(1320, 840, 160, 20, 'decor'),Decors(1520, 780, 80, 20, 'decor'),Decors(1680, 740, 200, 20, 'decor'),Decors(1360, 360, 240, 20, 'decor'),Decors(80, 700, 400, 420, 'decor'),Decors(880, 700, 480, 380, 'decor')]
-	,[]
+	Decors(-80, 700, 160, 400, 'decor'),Decors(320, 860, 240, 20, 'decor'),Decors(320, 560, 240, 20, 'decor'),Decors(640, 800, 160, 20, 'decor'),Decors(640, 460, 240, 20, 'decor'),Decors(800, 740, 160, 20, 'decor'),Decors(960, 380, 240, 20, 'decor'),Decors(1320, 840, 160, 20, 'decor'),Decors(1520, 780, 80, 20, 'decor'),Decors(1680, 740, 200, 20, 'decor'),Decors(1360, 360, 240, 20, 'decor'),Decors(80, 700, 400, 420, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(880, 700, 480, 380, 'decor')]
+	,[Ennemi([700,760],(40,40),"rond"),Ennemi([990,660],(40,40),"rond"),Ennemi([1245,610],(40,90),"ovale_haut")]
 	),
 #Niveau 2
 Levels([
-	Decors(-80, 700, 160, 380, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(1440, 780, 160, 20, 'decor'),Decors(1680, 780, 200, 20, 'decor'),Decors(1040, 800, 320, 300, 'decor'),Decors(960, 740, 240, 600, 'decor'),Decors(80, 700, 480, 480, 'decor'),Decors(960, 700, 160, 400, 'decor'),Decors(640, 620, 400, 480, 'decor'),Decors(240, 580, 480, 520, 'decor'),Decors(380, 540, 480, 600, 'decor')]
-	,[]
+	Decors(-80, 700, 160, 380, 'decor'),Decors(1440, 780, 160, 20, 'decor'),Decors(1680, 780, 200, 20, 'decor'),Decors(1040, 800, 320, 300, 'decor'),Decors(960, 740, 240, 600, 'decor'),Decors(80, 700, 480, 480, 'decor'),Decors(960, 700, 160, 400, 'decor'),Decors(640, 620, 400, 480, 'decor'),Decors(240, 580, 480, 520, 'decor'),Decors(1860, 700, 160, 400, 'decor'),Decors(380, 540, 480, 600, 'decor')]
+	,[Ennemi([290,540],(40,40),"rond"),Ennemi([495,500],(40,40),"rond"),Ennemi([710,450],(40,90),"ovale_haut"),Ennemi([1490,690],(40,90),"ovale_haut")]
 	)
 ]
 
@@ -125,7 +128,7 @@ def ajuste_niveaux_boss(levels,pos):
 
 
 nouveau_lvl = [
-niveau1
+niveaux[1]
 ]
 decors_de_base = [Decors(0,700,80,380,"decor"),Decors(1840,700,80,380,"decor")]
 niveau_vide = [Levels(decors_de_base,[])]
